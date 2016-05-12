6df868719e5174dfc7a4a860deec48e8cc23bb95
sobataro <sobatarooo@gmail.com>
2016-05-12 10:39:47 +0900

    create project

---
+++ ChinachuViewer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ChinachuViewer
+//
+//  Created by kosuke.matsuishi on 2016/05/12.
+//  Copyright © 2016年 松石 浩輔. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
