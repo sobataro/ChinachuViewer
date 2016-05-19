911e4907f3db8d0333a813209f11b30bb5d9ec6c
sobataro <sobatarooo@gmail.com>
2016-05-19 11:30:02 +0900

    add UITableView to RecordedViewController

--- ChinachuViewer/RecordedManager.swift
+++ ChinachuViewer/RecordedManager.swift
@@ -12,16 +12,17 @@ class RecordedManager {
     static let apiClientErrorDomain = "apiClientErrorDomain"
     static let jsonParseErrorCode = -100
 
-    static func getRecordedList(completionHandler: (recordedList: [Recorded]?, error: NSError) -> Void) {
+    static func getRecordedList(completionHandler: (recordedList: [Recorded]?, error: NSError?) -> Void) {
         RecordedSession.fetchRecordedList { (json, result) in
-            guard let json = json else {
+            guard let json = json?.array else {
                 completionHandler(recordedList: nil, error: NSError(domain: apiClientErrorDomain, code: jsonParseErrorCode, userInfo: nil))
                 return
             }
+
             let recordedList = json.map({ (json) -> Recorded in
                 Recorded.from(json.dictionaryObject!)!
             })
-            completionHandler(json: recordedList, error: nil)
+            completionHandler(recordedList: recordedList, error: nil)
         }
     }
 }
--- ChinachuViewer/RecordedViewController.swift
+++ ChinachuViewer/RecordedViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 import SwiftyJSON
 
-class RecordedViewController: UIViewController {
+class RecordedViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var recordedList: [Recorded]? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
 
-        RecordedSession.fetchRecordedList( { (json: JSON?, error: NSError?) in
-            print(json)
-        })
+        tableView.dataSource = self
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        RecordedManager.getRecordedList { (recordedList, error) in
+            self.recordedList = recordedList
+            print(recordedList)
+            self.tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +35,19 @@ class RecordedViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("RecordedCell", forIndexPath: indexPath)
 
+        guard let recorded = recordedList?[indexPath.row] else {
+            // FIXME
+            return cell
+        }
+        cell.textLabel?.text = recorded.fullTitle
+
+        return cell
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recordedList?.count ?? 0
+    }
 }
-
