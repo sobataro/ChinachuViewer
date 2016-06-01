8901e1bae9c5ff0219da712bf8add2dc590f8ee7
sobataro <sobatarooo@gmail.com>
2016-06-01 10:19:07 +0900

    add RecordedClientTests

--- ChinachuViewerTests/Clients/RecordedClientTest.swift
+++ ChinachuViewerTests/Clients/RecordedClientTest.swift
@@ -15,23 +15,11 @@ class RecordedClientTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
-
-        guard let path = NSBundle(forClass: RecordedClientTests.self).pathForResource("RecordedClientTest", ofType: "json") else {
-            XCTFail("Can't read the json file for test")
-            return
-        }
-        self.data = NSData(contentsOfFile: path)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-
-    func testExample() {
-        print(self.data)
-        XCTAssert(true)
     }
 
     func testRecordedEntity() {
@@ -49,10 +37,22 @@ class RecordedClientTests: XCTestCase {
                 }
                 XCTAssert(recordedList.count == 1)
                 let recorded = recordedList[0]
-                XCTAssert(recorded.program.id          == "bs151-ess")
-                XCTAssert(recorded.program.channelId   == "BS_151")
-                XCTAssert(recorded.program.channelName == "ＢＳ朝日１")
+                XCTAssert(recorded.tuner    == "PT3-S2")
+                XCTAssert(recorded.command  == "recpt1 --device /dev/pt3video1 --b25 --strip --sid 151 151 - -")
+                XCTAssert(recorded.filePath == "/tank/record/[151219-1854][BS151][PT3-S2]地球紀行　「BBCスペシャル フランスの大自然 動物たちの物語」.m2ts")
 
+                let program = recorded.program
+                XCTAssert(program.id          == "bs151-ess")
+                XCTAssert(program.channelId   == "BS_151")
+                XCTAssert(program.channelName == "ＢＳ朝日１")
+                XCTAssert(program.category    == "etc")
+                XCTAssert(program.title       == "地球紀行　「BBCスペシャル フランスの大自然 動物たちの物語」")
+                XCTAssert(program.subTitle    == "BBCスペシャル フランスの大自然 動物たちの物語")
+                XCTAssert(program.fullTitle   == "【二】地球紀行　「BBCスペシャル フランスの大自然 動物たちの物語」")
+                XCTAssert(program.detail      == "「アルプス・世界遺産の城 そしてプロバンスへ」▽美しくも雄大な自然に恵まれたフランス。アルプスの山々や世界遺産の城など雄大な自然の中で暮らす野生動物たちに密着！")
+                XCTAssert(program.start       == 1450518840000)
+                XCTAssert(program.end         == 1450526040000)
+                XCTAssert(program.seconds     == 7200)
 
                 expectation.fulfill()
             case .Failure:
@@ -61,7 +61,7 @@ class RecordedClientTests: XCTestCase {
             }
         }
         waitForExpectationsWithTimeout(3) { error in
-            XCTFail()
+            XCTAssertNil(error)
         }
     }
 }
