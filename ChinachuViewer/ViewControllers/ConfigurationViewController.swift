687d328fd5947f58ca290a09ee2f262b675e1ec5
sobataro <sobatarooo@gmail.com>
2016-06-06 10:23:10 +0900

    add ConfigurationViewController

--- ChinachuViewer/ViewControllers/ConfigurationViewController.swift
+++ ChinachuViewer/ViewControllers/ConfigurationViewController.swift
@@ -6,37 +6,34 @@
 //  Copyright © 2016年 松石 浩輔. All rights reserved.
 //
 
+import UIKit
 import Former
 
-class ConfigurationViewController: FormViewController {
+class ConfigurationViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    private lazy var former: Former = Former(tableView: self.tableView)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // FIXME: below codes are same as sample codes!
-        let labelRow = LabelRowFormer<FormLabelCell>()
-            .configure { row in
-                row.text = "Label Cell"
-            }.onSelected { row in
-                // Do Something
-        }
-        let inlinePickerRow = InlinePickerRowFormer<FormInlinePickerCell, Int>() {
-            $0.titleLabel.text = "Inline Picker Cell"
-            }.configure { row in
-                row.pickerItems = (1...5).map {
-                    InlinePickerItem(title: "Option\($0)", value: Int($0))
-                }
-            }.onValueChanged { item in
-                // Do Something
-        }
-        let header = LabelViewFormer<FormLabelHeaderView>() { view in
-            view.titleLabel.text = "Label Header"
-        }
-        let section = SectionFormer(rowFormer: labelRow, inlinePickerRow)
-            .set(headerViewFormer: header)
-        former.append(sectionFormer: section)
-
-        // TODO
         let configuration = Configuration.instance
-        print(configuration.serverAddress)
+
+        let serverAddressRow = TextFieldRowFormer<FormTextFieldCell>()
+            .configure { row in
+                row.text = configuration.serverAddress
+            }
+            .onUpdate { row in
+                configuration.serverAddress = row.text ?? ""
+        }
+
+        let serverSectionHeader = LabelViewFormer<FormLabelHeaderView>()
+            .configure { row in
+                row.text = "Server Settings"
+            }
+        let serverSection = SectionFormer(rowFormer: serverAddressRow)
+            .set(headerViewFormer: serverSectionHeader)
+
+        former.append(sectionFormer: serverSection)
+        former.endEditing()
     }
 }
